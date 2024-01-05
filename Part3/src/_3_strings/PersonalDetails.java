5c79ccc3914ef809e609438bdc320e5d28a56217
anilErbay <anilerbay@gmail.com>
2024-01-05 11:40:01 +0300

    check

--- Part3/src/_3_strings/PersonalDetails.java
+++ Part3/src/_3_strings/PersonalDetails.java
@@ -1,7 +1,34 @@
 package _3_strings;
 
+import java.util.Scanner;
+
 public class PersonalDetails {
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
 
+        int sum = 0;
+        int count = 0;
+        String longestName = "";
+
+        while (true) {
+            String input = scan.nextLine();
+            if (input.isEmpty()) {
+                break;
+            }
+
+            String[] parts = input.split(",");
+            // sum of the birth years, and count
+            sum += Integer.parseInt(parts[1]);
+            count++;
+
+            // check and assign the longest name
+            if (parts[0].length() > longestName.length()) {
+                longestName = parts[0];
+            }
+
+        }
+        System.out.println("Longest name: " + longestName);
+        System.out.println("Average of the birth years: " + (1.0 * sum / count));
+        scan.close();
     }
 }
